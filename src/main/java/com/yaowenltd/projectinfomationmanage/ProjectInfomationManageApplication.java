8f7a2a1187c2d18021037be61e4897208afc592c
wanglei <i.wanglei@foxmail.com>
2026-06-03 22:21:34 +0800

    init

---
+++ src/main/java/com/yaowenltd/projectinfomationmanage/ProjectInfomationManageApplication.java
@@ -0,0 +1,13 @@
+package com.yaowenltd.projectinfomationmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectInfomationManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectInfomationManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaowenltd/projectinfomationmanage/ProjectInfomationManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaowenltd.projectinfomationmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectInfomationManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
